72a9e9721ce129a7a44be78d0cc55cd3a06091b8
JulieABowers <Haldirolorien@comcast.net>
2024-01-31 17:34:36 -0600

    Code POST route for adding new notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,5 @@
 const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 
 //References a helper file that does the heavy lifting of CRUD operations
 //in the file system.
@@ -8,9 +9,32 @@ const {
   writeToFile,
 } = require('../helpers/fsUtils');
 
+//Declare it once. Use it for everything.
+const dbDestination = './db/db.json';
+
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile(dbDestination).then((data) => res.json(JSON.parse(data)));
 });
 
+// POST Route for a new note
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  console.log(req.body);
+
+  const { id, title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      id: uuidv4(),
+      title,
+      text
+    };
+
+    readAndAppend(newNote, dbDestination);
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Note not created');
+  }
+});
 module.exports = notes;
